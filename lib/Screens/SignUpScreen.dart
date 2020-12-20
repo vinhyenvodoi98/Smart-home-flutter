e6aaa8ea352cf1bdc992d631ac728e1c7e0cd01c
hoang <vinhyenvodoi98@gmail.com>
2020-12-20 13:05:30 +0700

    feat : connect to regiter api

--- lib/Screens/SignUpScreen.dart
+++ lib/Screens/SignUpScreen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/Screens/Home.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 class SignUpScreen extends StatefulWidget {
   @override
@@ -6,108 +10,164 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  bool _isLoading = false;
+
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+  final TextEditingController nameController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: ListView(
-        children: <Widget>[
-          BackButtonWidget(),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              children: <Widget>[
-                IconButton(icon: Icon(Icons.person), onPressed: null),
-                Expanded(
-                    child: Container(
-                        margin: EdgeInsets.only(right: 20, left: 10),
-                        child: TextField(
-                          decoration: InputDecoration(hintText: 'Username'),
-                        )))
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              children: <Widget>[
-                IconButton(icon: Icon(Icons.lock), onPressed: null),
-                Expanded(
-                    child: Container(
-                        margin: EdgeInsets.only(right: 20, left: 10),
-                        child: TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(hintText: 'Password'),
-                        )))
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              children: <Widget>[
-                IconButton(icon: Icon(Icons.mail), onPressed: null),
-                Expanded(
-                    child: Container(
-                        margin: EdgeInsets.only(right: 20, left: 10),
-                        child: TextField(
-                          decoration: InputDecoration(hintText: 'Email'),
-                        )))
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: <Widget>[
-                Radio(value: null, groupValue: null, onChanged: null),
-                RichText(
-                    text: TextSpan(
-                        text: 'I have accepted the',
-                        style: TextStyle(color: Colors.black),
-                        children: [
-                      TextSpan(
-                          text: 'Terms & Condition',
-                          style: TextStyle(
-                              color: Colors.teal, fontWeight: FontWeight.bold))
-                    ]))
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(5),
-              child: Container(
-                height: 60,
-                child: RaisedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, 'Home');
-                  },
-                  color: Color(0xFF00a79B),
-                  child: Text(
-                    'SIGN UP',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20),
+      body: Container(
+        child: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : ListView(
+                children: <Widget>[
+                  BackButtonWidget(),
+                  SizedBox(
+                    height: 20,
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
+                      children: <Widget>[
+                        IconButton(icon: Icon(Icons.mail), onPressed: null),
+                        Expanded(
+                            child: Container(
+                                margin: EdgeInsets.only(right: 20, left: 10),
+                                child: TextFormField(
+                                  controller: emailController,
+                                  decoration:
+                                      InputDecoration(hintText: 'Email'),
+                                )))
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
+                      children: <Widget>[
+                        IconButton(icon: Icon(Icons.lock), onPressed: null),
+                        Expanded(
+                            child: Container(
+                                margin: EdgeInsets.only(right: 20, left: 10),
+                                child: TextFormField(
+                                  controller: passwordController,
+                                  obscureText: true,
+                                  decoration:
+                                      InputDecoration(hintText: 'Password'),
+                                )))
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
+                      children: <Widget>[
+                        IconButton(icon: Icon(Icons.person), onPressed: null),
+                        Expanded(
+                            child: Container(
+                                margin: EdgeInsets.only(right: 20, left: 10),
+                                child: TextFormField(
+                                  controller: nameController,
+                                  decoration:
+                                      InputDecoration(hintText: 'Your Name'),
+                                )))
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: <Widget>[
+                        Radio(value: null, groupValue: null, onChanged: null),
+                        RichText(
+                            text: TextSpan(
+                                text: 'I have accepted the',
+                                style: TextStyle(color: Colors.black),
+                                children: [
+                              TextSpan(
+                                  text: 'Terms & Condition',
+                                  style: TextStyle(
+                                      color: Colors.teal,
+                                      fontWeight: FontWeight.bold))
+                            ]))
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(5),
+                      child: Container(
+                        height: 60,
+                        child: RaisedButton(
+                          onPressed:
+                              // emailController.text == "" ||
+                              //         passwordController.text == "" ||
+                              //         nameController.text == ""
+                              //     ? null
+                              //     :
+                              () {
+                            setState(() {
+                              _isLoading = true;
+                            });
+                            signUp(emailController.text,
+                                passwordController.text, nameController.text);
+                          },
+                          color: Color(0xFF00a79B),
+                          child: Text(
+                            'SIGN UP',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ),
-        ],
       ),
     );
+  }
+
+  signUp(String email, pass, name) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    Map data = {'email': email, 'password': pass, 'name': name, 'role': "1"};
+    print(data);
+    var jsonResponse;
+
+    var response =
+        await http.post("http://192.168.1.10:3000/users", body: data);
+    if (response.statusCode == 200) {
+      jsonResponse = json.decode(response.body);
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
+      if (jsonResponse != null) {
+        setState(() {
+          _isLoading = false;
+        });
+        sharedPreferences.setString("token", jsonResponse['token']);
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (BuildContext context) => Home()),
+            ModalRoute.withName('/Home'));
+      }
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+      print(response.body);
+    }
   }
 }
 
