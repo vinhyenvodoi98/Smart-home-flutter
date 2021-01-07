f1a15a8d768d3e1725293708520a47be0625ecdb
hoang <vinhyenvodoi98@gmail.com>
2021-01-07 22:41:06 +0700

    feat : use voice to turn light on

---
+++ lib/Components/SpeechScreen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+class SpeechScreen extends StatefulWidget {
+  @override
+  _SpeechScreenState createState() => _SpeechScreenState();
+}
+
+class _SpeechScreenState extends State<SpeechScreen> {
+  stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _text = 'Press the button and start speaking';
+  double _confidence = 1.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        animate: _isListening,
+        glowColor: Theme.of(context).primaryColor,
+        endRadius: 75.0,
+        duration: const Duration(milliseconds: 2000),
+        repeatPauseDuration: const Duration(milliseconds: 100),
+        repeat: true,
+        child: FloatingActionButton(
+          onPressed: _listen,
+          child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+        ),
+      ),
+    );
+  }
+
+  void _listen() async {
+    print(_isListening);
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val'),
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: (val) => setState(() {
+            _text = val.recognizedWords;
+            if (val.hasConfidenceRating && val.confidence > 0) {
+              _confidence = val.confidence;
+            }
+          }),
+        );
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
+  }
+}
--- lib/Screens/Room.dart
+++ lib/Screens/Room.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/Components/RoomDetails.dart';
+// import 'package:hello_world/Components/SpeechScreen.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'dart:convert';
 
 //ignore: must_be_immutable
 class Room extends StatefulWidget {
@@ -10,10 +16,32 @@ class Room extends StatefulWidget {
 }
 
 class _RoomState extends State<Room> {
+  stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _text = 'Press the button and start speaking';
+
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff202227),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        animate: _isListening,
+        glowColor: Theme.of(context).primaryColor,
+        endRadius: 75.0,
+        duration: const Duration(milliseconds: 2000),
+        repeatPauseDuration: const Duration(milliseconds: 100),
+        repeat: true,
+        child: FloatingActionButton(
+          onPressed: _listen,
+          child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+        ),
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
@@ -42,9 +70,51 @@ class _RoomState extends State<Room> {
                 )),
           ),
           SizedBox(height: 50),
-          Expanded(child: RoomDetails())
+          Expanded(child: RoomDetails()),
         ],
       ),
     );
+  }
+
+  void _listen() async {
+    print(_isListening);
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val'),
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: (val) => setState(() {
+            _text = val.recognizedWords;
+            print(_text);
+            if (_text == 'Turn on the lamp' || _text == "Bật đèn lên") {
+              _text = '';
+              turnLamp("1");
+            }
+            if (_text == 'Turn off the lamp' || _text == "Tắt đèn đi") {
+              _text = '';
+              turnLamp("0");
+            }
+          }),
+        );
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
+  }
+
+  turnLamp(String control) async {
+    var jsonResponse;
+    Map data = {'control': control};
+    var response =
+        await http.post(DotEnv().env['IBM_CLOUD'] + "/led", body: data);
+    if (response.statusCode == 200) {
+      jsonResponse = json.decode(response.body);
+      print('Response : ${response.body}');
+    }
+    print(jsonResponse);
   }
 }
